5ee91bd9c4a1220cd8d771e52a9d447ee10713b1
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2021-09-27 08:19:02 -0300

    classname

--- components/header.js
+++ components/header.js
@@ -4,11 +4,11 @@ export default function Header() {
   const [isVisibleMenu, setIsVisibleMenu] = useState(true);
   return (
     <>
-      <div class="h-24 z-50 relative container mx-auto px-6 grid grid-cols-3">
-        <div class="flex items-center">
+      <div className="h-24 z-50 relative container mx-auto px-6 grid grid-cols-3">
+        <div className="flex items-center">
           <button onClick={() => setIsVisibleMenu(!isVisibleMenu)}>
             <svg
-              class="w-8 h-8 text-white"
+              className="w-8 h-8 text-white"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -24,15 +24,15 @@ export default function Header() {
           </button>
 
           <div
-              class={ isVisibleMenu ? 'hidden': '' +'fixed inset-0 w-full h-full bg-white z-50 text-yellow-900'}
+              className={ isVisibleMenu ? 'hidden': '' +'fixed inset-0 w-full h-full bg-white z-50 text-yellow-900'}
             >
-              <div class="container h-full mx-auto px-6 py-8 relative z-10 flex flex-col items-center justify-center text-2xl uppercase font-bold tracking-widest space-y-6">
+              <div className="container h-full mx-auto px-6 py-8 relative z-10 flex flex-col items-center justify-center text-2xl uppercase font-bold tracking-widest space-y-6">
                 <button
                   onClick={() => setIsVisibleMenu(!isVisibleMenu)}
-                  class="absolute top-0 left-0 mt-8 ml-6"
+                  className="absolute top-0 left-0 mt-8 ml-6"
                 >
                   <svg
-                    class="w-8 h-8"
+                    className="w-8 h-8"
                     fill="none"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
@@ -49,28 +49,28 @@ export default function Header() {
 
                 <a
                   href="#"
-                  class="inline-block border-b-4 border-transparent hover:border-yellow-900"
+                  className="inline-block border-b-4 border-transparent hover:border-yellow-900"
                 >
                   Link
                 </a>
               </div>
-              <div class="absolute inset-0 w-full h-full bg-yellow-900 bg-opacity-20"></div>
+              <div className="absolute inset-0 w-full h-full bg-yellow-900 bg-opacity-20"></div>
             </div>
         </div>
 
-        <div class="flex items-center justify-center">
+        <div className="flex items-center justify-center">
           <a
             href="/"
-            class="text-white uppercase font-bold text-2xl tracking-widest"
+            className="text-white uppercase font-bold text-2xl tracking-widest"
           >
-            <h2 className="font-display font-bold text-5xl mb-6">Crypto Blog</h2>
+            <h2 classNameName="font-display font-bold text-5xl mb-6">Crypto Blog</h2>
           </a>
         </div>
 
-        <div class="flex items-center justify-end">
+        <div className="flex items-center justify-end">
           <a href="/contact">
             <svg
-              class="w-8 h-8 text-white"
+              className="w-8 h-8 text-white"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -87,7 +87,7 @@ export default function Header() {
         </div>
       </div>
 
-      <div class="w-full h-24 bg-yellow-900 bg-opacity-95 absolute top-0 left-0"></div>
+      <div className="w-full h-24 bg-yellow-900 bg-opacity-95 absolute top-0 left-0"></div>
     </>
   );
 }
