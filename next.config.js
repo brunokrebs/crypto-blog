144e642bd888034b78024238c45763dae660cd96
Bruno Krebs <krebs.bruno@gmail.com>
2021-10-03 22:06:53 -0300

    fine;

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-const withMDX = require("@next/mdx")({
-  pageExtensions: ["js", "jsx", "md", "mdx"],
+const withMDX = require('@next/mdx')({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
   },
-});
+})
 
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["ik.imagekit.io"],
+    domains: ['ik.imagekit.io', 'cdn.pixabay.com'],
   },
   withMDX,
 
   webpack: (config) => {
-    config.experiments = { topLevelAwait: true };
-    return config;
+    config.experiments = { topLevelAwait: true }
+    return config
   },
-};
+}
